d6c1a730dd9269a7df2ba39381442ab2a43cc942
Mschelley <20212026@nbsc.edu.ph>
2026-04-07 12:32:32 +0800

    updated project

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonFab, IonFabButton, IonFabList, IonHeader, IonIcon, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
+import { document,chevronUpCircle, colorPalette, globe } from 'ionicons/icons';
 
 const Home: React.FC = () => {
   return (
@@ -16,7 +17,28 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+
+
+       <IonFab slot="fixed" vertical="bottom" horizontal="end">
+                 <IonFabButton>
+                   <IonIcon icon={chevronUpCircle}></IonIcon>
+                 </IonFabButton>
+                 <IonFabList side="top">
+                   <IonFabButton>
+                     <IonIcon icon={document}></IonIcon>
+                   </IonFabButton>
+                   <IonFabButton>
+                     <IonIcon icon={colorPalette}></IonIcon>
+                   </IonFabButton>
+                   <IonFabButton>
+                     <IonIcon icon={globe}></IonIcon>
+                   </IonFabButton>
+                 </IonFabList>
+        </IonFab>
+
+        <ExploreContainer></ExploreContainer>
+      
+      
       </IonContent>
     </IonPage>
   );
--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,15 +1,55 @@
-import {  IonHeader, IonPage, IonTitle, IonToolbar} from '@ionic/react';
+import {  IonMenuButton, IonButtons, IonHeader, IonPage, IonTitle, IonToolbar, IonSplitPane, IonMenu, IonContent, IonMenuToggle, IonItem, IonIcon, IonRouterOutlet } from '@ionic/react';
+import { Route, Redirect } from 'react-router-dom';
+import { homeOutline, settingsOutline, informationOutline} from 'ionicons/icons';
+import Page1 from './Page1';
+import Page2 from './Page2';
+import Home from './Home';
 
- const Menu : React.FC = () => {
+
+
+ const Menu: React.FC<{}> = () => {
+    const path=[
+        {name: 'Home', url: '/app/home', icon: homeOutline},
+        {name: 'Page 1', url: '/app/page1', icon: settingsOutline},
+        {name: 'Page 2', url: '/app/page2', icon: informationOutline}
+    ]
     return(
         <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonTitle>Menu</IonTitle>
-                </IonToolbar>
-            </IonHeader>
+
+        <IonSplitPane contentId="main">
+            <IonMenu contentId="main">
+                <IonHeader>
+                    <IonToolbar>
+                        <IonTitle>Menu</IonTitle>
+                        <IonButtons slot="start">
+              <IonMenuButton></IonMenuButton>
+          </IonButtons>
+                    </IonToolbar>
+                   
+                </IonHeader>
+
+                <IonContent>
+                    {/* Sidemenu Items */}
+                    {path.map((item, index) => (
+                        <IonMenuToggle key={index}>
+                            <IonItem routerLink={item.url} routerDirection="forward">
+                                <IonIcon icon={item.icon} slot="start"></IonIcon>
+                                {item.name}
+                            </IonItem>
+                        </IonMenuToggle>
+                    ))}
+                </IonContent>
+
+            </IonMenu>
+            <IonRouterOutlet id="main">
+                <Route exact path="/app/home" component={Home} />
+                <Route exact path="/app/page1" component={Page1} />
+                <Route exact path="/app/page2" component={Page2} />
+                <Redirect from="/app" to="/app/home" exact />
+            </IonRouterOutlet>
+        </IonSplitPane>
            </IonPage>
     );
- }; 
+ };
 
  export default Menu;
